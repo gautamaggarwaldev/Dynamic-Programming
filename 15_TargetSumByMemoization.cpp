162cfa44532ca8edcb231b0aed7030ae7ce80d68
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-07-05 23:28:04 +0530

    target sum exactly same with partition with given diff by memoization

---
+++ 15_TargetSumByMemoization.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int findTargetSumWaysMemo(vector<int> &nums, int sum, int n, vector<vector<int>> &dp)
+    {
+        if (n == 0)
+        {
+            if (sum == 0)
+                return 1;
+            else
+                return 0;
+        }
+        if (dp[n][sum] != -1)
+        {
+            return dp[n][sum];
+        }
+        if (sum >= nums[n - 1])
+        {
+            dp[n][sum] = findTargetSumWaysMemo(nums, sum - nums[n - 1], n - 1, dp) + findTargetSumWaysMemo(nums, sum, n - 1, dp);
+        }
+        else
+        {
+            dp[n][sum] = findTargetSumWaysMemo(nums, sum, n - 1, dp);
+        }
+        return dp[n][sum];
+    }
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int Tsum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            Tsum += nums[i];
+        }
+        if ((Tsum + target) % 2 != 0 || Tsum < abs(target)) 
+        {
+            return 0; // IMP
+        }
+        int sum = (Tsum + target) / 2;
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, -1));
+        int ans = findTargetSumWaysMemo(nums, sum, n, dp);
+        return ans;
+    }
+};
+int main()
+{
+}
