5b9b4a039c5c021a7bd30cbf316cb54bab198fd9
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-07-06 12:54:10 +0530

    coin change in which count no. of ways to make amount using coins

--- 22_CoinChange-NumberofWaysByRecursion.cpp
+++ 22_CoinChange-NumberofWaysByRecursion.cpp
@@ -6,10 +6,12 @@ class Solution
 public:
     int coinChangeRec(vector<int> &coins, int amount, int n)
     {
+        // Base cases
         if (amount == 0)
             return 1;
         if (n == 0)
             return 0;
+    
         if (amount >= coins[n - 1])
         {
             return coinChangeRec(coins, amount - coins[n - 1], n) + coinChangeRec(coins, amount, n - 1);
