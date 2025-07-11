c5ff45ccbcdbab0614325ca9e1b903515eaf7ac6
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-07-11 22:48:54 +0530

    matrix chain multiplication using recursion

--- 56_MatrixChainMultiplicationByRecursion.cpp
+++ 56_MatrixChainMultiplicationByRecursion.cpp
@@ -13,6 +13,12 @@ public:
         int mn = INT_MAX;
         for (int k = i; k <= j - 1; k++)
         {
+            // arr[i-1] * arr[k] * arr[j] is the cost of multiplying the two matrices
+            // formed by the indices i to k and k+1 to j
+            // mcm(arr, i, k) is the cost of multiplying the first matrix
+            // mcm(arr, k + 1, j) is the cost of multiplying the second matrix
+            // The total cost is the sum of these two costs plus the cost of multiplying the two resulting matrices
+            // together.
             int tempAns = mcm(arr, i, k) + mcm(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
 
             if (tempAns < mn)
